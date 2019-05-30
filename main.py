Added MSE, sMAPE and correct scaling factor

main.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 
         realTrainLoader, realTestLoader = sf.realset_generator(file_path, 0.5, dataloader_params, lower_boundary, upper_boundary)
 
-        scale_factor = sf.get_scaling_factor(realTrainLoader)
+        scale_factor = sf.get_scaling_factor(realTrainLoader, realTestLoader)
         scaled_real_trainLoader = sf.scale(realTrainLoader, scale_factor)
         scaled_real_testloader = sf.scale(realTestLoader, scale_factor)
         # scaled_real_trainLoader = sf.rescale(scaled_real_trainLoader)
@@ -64,7 +64,8 @@ if __name__ == '__main__':
         # net = sf.train(net, criterion, trainloader, scaled_real_testloader, optimizer)
 
         print("scaled factor: {}".format(scale_factor))
-        print(scaled_real_testloader.labels)
+        sf.print_dataset(scaled_real_testloader)
+        #print(scaled_real_testloader)
         # todo i might be doubly dividing the error  by count, ensure  this isn't the case
         # prediction_criterion = nn.MSELoss()
         # loss, count, predictions = sf.predict_scenario(net, criterion, scaled_real_testloader)
@@ -75,17 +76,19 @@ if __name__ == '__main__':
         # print(sf.get_generalized_approximation(x_values).reshape)
         clf = MLPRegressor(solver='adam', alpha=0.01, hidden_layer_sizes = (100), random_state = 1, max_iter=20000)
 
+
         real_x_train_values = torch.tensor(scaled_real_trainLoader.list_IDs).reshape(-1,1)
         real_y_train_values = torch.tensor(scaled_real_trainLoader.labels).reshape(-1,1)
 
-        print(real_y_train_values)
+       # print(real_y_train_values)
 
         clf.fit(x_values.reshape(-1,1), sf.get_generalized_approximation(x_values).reshape(-1,1))
         clf.fit(real_x_train_values, real_y_train_values)
 
         test_x_values = torch.tensor(scaled_real_testloader.list_IDs).detach().numpy()
         predictions = clf.predict(test_x_values.reshape(-1,1))
-        print(predictions)
+        print("TRAINING COMPETE")
+        #print(predictions)
         # print(predictions)
         # predictions = predictions.detach().numpy()
 
@@ -97,29 +100,23 @@ if __name__ == '__main__':
         sorted_x_values = []
         sorted_predictions = []
         sorted_true_y_values = []
-        print(x_values)
+        #print(x_values)
         for index in indexes:
             sorted_x_values.append(real_x_values[index])
             sorted_predictions.append(predictions[index])
             sorted_true_y_values.append(true_y_values[index])
 
-        print(list(zip(sorted_x_values, sorted_predictions)))
-
-
+        print(list(zip(sorted_x_values, sorted_predictions, sorted_true_y_values)))
+        print("MSE: ", sf.MSE(sorted_predictions, sorted_true_y_values))
+        print("SMAPE: ", sf.SMAPE(sorted_predictions, sorted_true_y_values))
+        print("MSE LJ: ", sf.MSE(sf.get_LJ_list(sorted_x_values), sorted_true_y_values))
+        print("SMAPE LJ: ", sf.SMAPE(sf.get_LJ_list(sorted_x_values), sorted_true_y_values))
 
         plt.plot(sorted_x_values, sorted_predictions, label="predicted values")
         plt.plot(sorted_x_values, sorted_true_y_values, label="true data")
         plt.plot(sorted_x_values, sf.get_generalized_approximation(numpy.array(sorted_x_values)), label="generalized function")
-        # plt.plot(sorted_x_values, )
+        plt.plot(sorted_x_values, )
         plt.xlabel("Range [A]")
         plt.ylabel("force deviation from mean [eV]")
         plt.legend()
         plt.show()
-
-    # print("average loss over {} runs is {}".format(runs, numpy.mean(accuracies)))
-
-
-
-
-
-

supporting_functions.py
@@ -23,17 +23,23 @@ def uniform_random_n(start, stop, n):
     return numpy.random.uniform(start, stop, n)
 
 
-def get_scaling_factor(DataLoader, feature_index=0):
+def get_scaling_factor(DataLoader, TestLoader, feature_index=0):
     # returns the factor by which the iterator needs to be scaled to achieve zero weighted scaling
-    # sum = 0
-    # count = 0
-    # for i, data in enumerate(DataLoader):
-    #     features, label = data
-    #     sum += label
-    #     count += 1
-    #
-    # return sum/count
-    return -2952.11835930275
+     scale = 0
+     distance = 0
+     for i, data in enumerate(DataLoader):
+         features, label = data
+         if features > distance:
+             distance = features
+             scale = label
+     for i, data in enumerate(TestLoader):
+         features, label = data
+         if features > distance:
+             distance = features
+             scale = label
+  
+     return float(scale)    
+  
 
 
 def scale(DataLoader, scaling_factor, feature_index=0):
@@ -89,9 +95,9 @@ def numpy_data_to_trainloaders(numpy_data, train_ratio, dataloader_params):
 
     numpy.random.shuffle(numpy_data)
 
-    numpy_train = numpy_data[:train_objects]
+    numpy_train = numpy_data[:int(train_objects)]
     # numpy_validate = numpy_data[train_objects:(train_objects+validate_objects)]
-    numpy_test = numpy_data[train_objects:]
+    numpy_test = numpy_data[int(train_objects):]
 
     partition_train, labels_train = numpy_to_x_y(numpy_train)
     # partition_validate, labels, validate = nu
@@ -203,3 +209,34 @@ def predict_scenario(net, criterion, testloader):
     # print(all_inputs)
     all_predictions = net(torch.tensor(all_inputs))
     return total_loss, i+1, all_predictions
+
+def print_dataset(dataset):
+    for i, data in enumerate(dataset):
+        features, label = data
+        print(float(features), float(label))
+
+def get_LJ(x):
+    return 4*3*((1/x)**12-(1/x)**6)
+
+def get_LJ_list(x_values):
+    y_values = []
+    for x in x_values:
+        y_values.append(get_LJ(x))
+    return y_values
+
+def MSE(New, Old):
+    sum = 0
+    for x in range(len(New)):
+        sum += (New[x]+Old[x])**2
+
+    return sum/len(New)
+
+def SMAPE(New, Old):
+    sum = 0
+    for x in range(len(New)):
+        sum += abs(Old[x]-New[x])/((New[x]+Old[x])/2)
+
+    return sum/len(New)
+
+
+