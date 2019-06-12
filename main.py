fb5df2bbc9547a49feb1c6833a1d062b6f7d1634
David Vojtek <david.vojtek1@vsb.cz>
2019-06-12 11:45:18 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import supporting_functions as sf
 
+import time
 import torch
 import torch.nn as nn
 import numpy
@@ -17,10 +18,24 @@ if __name__ == '__main__':
     runs = 1
     lower_boundary = 0.85
     upper_boundary = 3
+    max_iteration = 200000
     learning_rate = 0.01
-    x_values = sf.uniform_random_n(lower_boundary, upper_boundary, 30000)
+    num = 0;
+    MSE = 10;
+    Results = [];
+    x_test = [0.9, 0.95, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 ,1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 5, 10, 50]
+    solvers = [ "sgd","adam", "lbfgs"]
+    activations = ["logistic", "relu", "tanh"]
+    hidden_neurons = [50,100,150,200,250]
+    x_values = sf.uniform_random_n(lower_boundary, upper_boundary, 350)
     file_path = 'data/sigmau-.csv'
     true_data_epochs = 100
+    timer=0
+    timer2 = 0;
+    MSEr = [0,0,0,0,0]
+    MSElj = [0,0,0,0,0]
+    sMAPEr = [0,0,0,0,0]
+    sMAPElj = [0,0,0,0,0]
     dataloader_params = {
         'batch_size': 1,
         'shuffle': False,
@@ -30,21 +45,7 @@ if __name__ == '__main__':
     predictions = []
 
 
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-
-            self.fc1 = nn.Linear(1, 10)
-            # self.fc2 = nn.Linear(100, 100)
-            self.fc3 = nn.Linear(10, 1)
-
-
-        def forward(self, x):
-            x = x.view(-1, 1)
-            x = F.relu(self.fc1(x))
-            # x = self.fc2(x)
-            x = F.relu(self.fc3(x))
-            return x
+   
 
     for i in range(runs):
         # trainloader, testloader = sf.datasets_generator(x_values, sf.get_generalized_approximation(x_values), 0.8, dataloader_params)
@@ -74,49 +75,98 @@ if __name__ == '__main__':
         # print("average loss is: {}".format(loss / count))
         # accuracies.append(float(loss / count))
         # print(sf.get_generalized_approximation(x_values).reshape)
-        clf = MLPRegressor(solver='adam', alpha=0.01, hidden_layer_sizes = (100), random_state = 1, max_iter=20000)
+     
+        for s in range(1):
+            for a in range(1):
+                for n in range(5):
+                    for m in range(5):
+                        timer2 = time.time()
+                        for p in range(5):
+                            layers = (hidden_neurons[n],hidden_neurons[m])
+                            clf = MLPRegressor(solver=solvers[s], activation=activations[a], alpha=0.01, hidden_layer_sizes = layers, learning_rate_init=0.01,  random_state = 1, max_iter=max_iteration)
+                            print(clf)
+                            
+                            real_x_train_values = torch.tensor(scaled_real_trainLoader.list_IDs).reshape(-1,1)
+                            real_y_train_values = torch.tensor(scaled_real_trainLoader.labels).reshape(-1,1)
 
+                           # print(real_y_train_values)
+                            timer = time.time()
+                            while(MSE > 0.05 and num < 2000):
+                                for q in range(50):
+                                    clf.partial_fit(x_values.reshape(-1,1), sf.get_LJ(x_values).reshape(-1,1))
+                                predictions = clf.predict(x_values.reshape(-1,1))
+                                listes = sf.get_LJ_list(x_values.reshape(-1,1))
+                                MSE = sf.MSE(predictions, listes)
+                                num += +1
+                                print(num, MSE)
+                            for x in range(len(predictions)):
+                                print(float(x_values.reshape(-1,1)[x]), predictions[x], float(listes[x]), predictions[x]-float(listes[x]))
+                            
+                                
+                            print("Pretraining", time.time()-timer, "s ", MSE)
+                            timer = time.time()
+                            MSE=10
+                            num = 0
+                            #time.sleep(30)
+                            while(MSE > 0.01 and num<5000):
+                                for q in range(20):
+                                    clf.partial_fit(real_x_train_values, real_y_train_values)
+                                predictions = clf.predict(real_x_train_values)
+                                MSE = sf.MSE(predictions, real_y_train_values)
+                                num +=1
+                                print(num, MSE)
+                            print("Training", time.time()-timer, "s ", MSE)
+                            
+                            test_x_values = torch.tensor(scaled_real_testloader.list_IDs).detach().numpy()
+                            predictions = clf.predict(test_x_values.reshape(-1,1))
+                           # print("TRAINING COMPLETE")
+                            #print(predictions)
+                            # print(predictions)
+                            # predictions = predictions.detach().numpy()
 
-        real_x_train_values = torch.tensor(scaled_real_trainLoader.list_IDs).reshape(-1,1)
-        real_y_train_values = torch.tensor(scaled_real_trainLoader.labels).reshape(-1,1)
+                            real_x_values = torch.tensor(scaled_real_testloader.list_IDs).detach().numpy()
+                            true_y_values = torch.tensor(scaled_real_testloader.labels).detach().numpy()
+                            # print(x_values)
 
-       # print(real_y_train_values)
+                            indexes = numpy.argsort(real_x_values)
+                            sorted_x_values = []
+                            sorted_predictions = []
+                            sorted_true_y_values = []
+                            #print(x_values)
+                            for index in indexes:
+                                sorted_x_values.append(real_x_values[index])
+                                sorted_predictions.append(predictions[index])
+                                sorted_true_y_values.append(true_y_values[index])
+                            MSEr[p] = sf.MSE(sorted_predictions, sorted_true_y_values)
+                            MSElj[p] = sf.MSE(sf.get_LJ_list(sorted_x_values), sorted_true_y_values)
+                            sMAPEr[p] = sf.SMAPE(sorted_predictions, sorted_true_y_values)
+                            sMAPElj[p] = sf.SMAPE(sf.get_LJ_list(sorted_x_values), sorted_true_y_values)
+                            for j in x_test:
+                                print(j, float(clf.predict(j)), float(sf.get_LJ(j)))
+                            print("//////////////////////////")
+                            print(list(zip(sorted_x_values, sorted_predictions, sorted_true_y_values)))
+                            print("MSE: ", MSEr[p])
+                            print("SMAPE: ", sMAPEr[p])
+                            print("MSE LJ: ", MSElj[p])
+                            print("SMAPE LJ: ", sMAPElj[p])
+                            if p==4:
+                                print("****************************************************************************************************")
+                                #print(clf)
+                                #print("MSE", sum(MSEr)/len(MSEr))
+                                #print("sMAPE", sum(sMAPEr)/len(sMAPEr))
+                                #print("MSE", sum(MSElj)/len(MSElj))
+                                #print("sMAPE", sum(sMAPElj)/len(sMAPElj))
+                                #print("AVERAGE TIME:", (time.time()-timer2)/5)
+                                Results.append([solvers[s],activations[a], hidden_neurons[n], hidden_neurons[m], sum(MSEr)/len(MSEr), sum(sMAPEr)/len(sMAPEr), sum(MSElj)/len(MSElj),(time.time()-timer2)/5])
+                                print(solvers[s],activations[a], hidden_neurons[n], hidden_neurons[m], sum(MSEr)/len(MSEr), sum(sMAPEr)/len(sMAPEr), sum(MSElj)/len(MSElj),(time.time()-timer2)/5)
 
-        clf.fit(x_values.reshape(-1,1), sf.get_generalized_approximation(x_values).reshape(-1,1))
-        clf.fit(real_x_train_values, real_y_train_values)
-
-        test_x_values = torch.tensor(scaled_real_testloader.list_IDs).detach().numpy()
-        predictions = clf.predict(test_x_values.reshape(-1,1))
-        print("TRAINING COMPETE")
-        #print(predictions)
-        # print(predictions)
-        # predictions = predictions.detach().numpy()
-
-        real_x_values = torch.tensor(scaled_real_testloader.list_IDs).detach().numpy()
-        true_y_values = torch.tensor(scaled_real_testloader.labels).detach().numpy()
-        # print(x_values)
-
-        indexes = numpy.argsort(real_x_values)
-        sorted_x_values = []
-        sorted_predictions = []
-        sorted_true_y_values = []
-        #print(x_values)
-        for index in indexes:
-            sorted_x_values.append(real_x_values[index])
-            sorted_predictions.append(predictions[index])
-            sorted_true_y_values.append(true_y_values[index])
-
-        print(list(zip(sorted_x_values, sorted_predictions, sorted_true_y_values)))
-        print("MSE: ", sf.MSE(sorted_predictions, sorted_true_y_values))
-        print("SMAPE: ", sf.SMAPE(sorted_predictions, sorted_true_y_values))
-        print("MSE LJ: ", sf.MSE(sf.get_LJ_list(sorted_x_values), sorted_true_y_values))
-        print("SMAPE LJ: ", sf.SMAPE(sf.get_LJ_list(sorted_x_values), sorted_true_y_values))
-
-        plt.plot(sorted_x_values, sorted_predictions, label="predicted values")
-        plt.plot(sorted_x_values, sorted_true_y_values, label="true data")
-        plt.plot(sorted_x_values, sf.get_generalized_approximation(numpy.array(sorted_x_values)), label="generalized function")
-        plt.plot(sorted_x_values, )
-        plt.xlabel("Range [A]")
-        plt.ylabel("force deviation from mean [eV]")
-        plt.legend()
-        plt.show()
+                            #plt.plot(sorted_x_values, sorted_predictions, label="predicted values")
+                            #plt.plot(sorted_x_values, sorted_true_y_values, label="true data")
+                            #plt.plot(sorted_x_values, sf.get_generalized_approximation(numpy.array(sorted_x_values)), label="generalized function")
+                            #plt.plot(sorted_x_values, )
+                            #plt.xlabel("Range [A]")
+                            #plt.ylabel("force deviation from mean [eV]")
+                            #plt.legend()
+                            #plt.show()
+    print("-------------------------------------------------------------------------------------------------------------")
+    print(Results)
